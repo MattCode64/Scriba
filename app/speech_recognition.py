46c873965e12cf1e5161466fbdb4093679d430c4
Matthieu <matthieu.freireeleuterio.stagiaire@elysee.fr>
2024-03-05 15:08:52 +0100

    _

--- app/speech_recognition.py
+++ app/speech_recognition.py
@@ -1,21 +1,26 @@
 import whisper
+import numpy as np
 from pydub import AudioSegment
 
 
 def load_audio_file(audio_path):
     audio = AudioSegment.from_mp3(audio_path)
-    return audio.raw_data
+    samples = np.array(audio.get_array_of_samples())
+    if audio.channels == 2:
+        samples = samples.reshape((-1, 2))
+    return samples.astype(np.float32), audio.frame_rate
 
 
 def load_model(model_name="base"):
     return whisper.load_model(model_name)
 
 
-def transcribe(model, audio):
+def transcribe(model, audio) -> str:
     options = {
         "language": "fr",  # Specify the French language
     }
     print("Transcribing audio...")
+    print(f"Audio type: {type(audio)}")
     text_transcribe = model.transcribe(audio, **options, fp16=False, verbose=True)
     return text_transcribe["text"]
 
